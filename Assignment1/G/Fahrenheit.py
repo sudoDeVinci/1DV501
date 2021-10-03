ce155781dd5b72f5292d12e161a2a59778d00b08
devinci <hervorarvitr@gmail.com>
2021-10-03 12:44:26 +0000

    _

--- Assignment1/G/Fahrenheit.py
+++ Assignment1/G/Fahrenheit.py
@@ -1,30 +1,29 @@
 __author__ = "Tadj Cazaubon"
 
 """
-Program will find the celsius equivalent to a user provided temperature in fahrenheit.
+Program will find the celsius equivalent to
+a user provided temperature in fahrenheit
 Revised 07/09/21
 """
 
 fahrenheit = input("\n>Enter temp in farenheit: ")
 
-#if input cannot be converted to float, request different input
+# if input cannot be converted to float, request different input
 try:
     float(fahrenheit)
 
 except Exception:
 
-    #isinstance checks if a variable is of a specified datatype 
-    while isinstance(fahrenheit,float)==False:
-        fahrenheit = input("\n>Enter temp in farenheit [Must be floating point]: ")
+    # isinstance checks if a variable is specified datatype
+    while isinstance(fahrenheit, float) is False:
+        fahrenheit = input("\n>Enter temp in farenheit [Must be float]: ")
         try:
             float(fahrenheit)
             break
-        except:
+        except Exception:
             continue
 
-#Calculate celsius equivalent
+# Calculate celsius equivalent
 celsius = 5*(float(fahrenheit)-32)/9
 
 print(f"\n> Corresponding temperature in Celsius is: {celsius} ")
-
-print("_")
